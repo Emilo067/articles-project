ded9208a001008f30dae121711d0e625d2a47976
Emilo067 <gasanovemil467@gmail.com>
2025-10-29 09:26:16 +0300

    create Sidebar component

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Sidebar.module.scss';
+import {useState} from "react";
+import {Button, LangSwitcher, ThemeSwitcher} from "shared";
+
+interface SidebarProps {
+    className?: string;
+}
+
+export const Sidebar = ( props: SidebarProps) => {
+    const [isCollapsed, setIsCollapsed] = useState(false)
+
+    const { className } = props;
+
+    const onToggle = () => {
+        setIsCollapsed(prev => !prev)
+    }
+
+    return (
+        <div className={classNames(cls.Sidebar, {[cls.collapsed]: isCollapsed}, [className])}>
+            <Button onClick={onToggle}>toggle</Button>
+            <div className={cls.switchers}>
+                <ThemeSwitcher/>
+                <LangSwitcher className={cls.lang}/>
+            </div>
+        </div>
+    );
+};
